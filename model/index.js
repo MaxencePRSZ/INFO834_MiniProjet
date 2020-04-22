1f0bb3930c57a92bfe1e3d78de9d9125656b49ed
MaxencepRSZ <maxenceperso@gmail.com>
2020-04-22 14:39:12 +0200

    Ajout de commentaire et de méthodes de récupération de mongo

--- model/index.js
+++ model/index.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema;
 
-
+//Schéma de message, qui correspond à la structure de la bdd...
 var SchemaMessage = new Schema({
     User: String,
     Message: String,
@@ -10,6 +10,7 @@ var SchemaMessage = new Schema({
 
 var Message = mongoose.model('Message', SchemaMessage)
 
+//Récupération du nombre de message d'un utilisateur
 function getUsersMessageNumbers(username, func) {
     var ret = 0
     Message.aggregate([{ $match: { User: username } }, { $sortByCount: "$User" }], function (err, result) {
@@ -20,7 +21,7 @@ function getUsersMessageNumbers(username, func) {
     });
 }
 
-
+//Récupération de tous les messages d'un users spécifique
 function getAllMessagesFromUser(username) {
     // Pour avoir le nombre de message total (toutes rooms confondues) d'un utilisateur
     Message.aggregate([{ $match: { User: username } }, { $sortByCount: "$User" }], function (err, result) {
@@ -28,7 +29,7 @@ function getAllMessagesFromUser(username) {
     })
 }
 
-//Get last Salon
+//Récupération du dernier salon, c'est à dire le salon avec l'ID la plus grande
 function getLastSalon(func){
     Message.find({}, { Salon : 1 }).sort({Salon : -1}).limit(1).exec( function(err, result) {
         if (err) throw err;
@@ -38,7 +39,28 @@ function getLastSalon(func){
    });
 }
 
+//Récupération de tous les messages d'un utilisateur dans un salon spécifique
+function getMessageFromUserInSalon(username, salon, func){
+    Message.find({User : username, Salon : salon}).exec(function(err, result){
+        if (err) throw err;
+        if (result !== undefined)
+            ret = result;
+        func(ret);
+    });
+}
 
+//Récupération de tous les messages d'un salon spécifique
+function getMessageFromSalon( salon, func){
+    Message.find({Salon : salon}).exec(function(err, result){
+        if (err) throw err;
+        if (result !== undefined)
+            ret = result;
+        func(ret);
+    });
+}
+
+
+//Récupération de tous les utilisateurs qui ont particité à une discussion
 function getAllUsers() {
     Message.aggregate([{ $group: { _id: "$User" } }], function (err, result) {
         console.log(result.la);
@@ -51,6 +73,10 @@ function getAllUsers() {
 module.exports = {
     Message: Message,
     getUsersMessageNumbers: getUsersMessageNumbers,
-    getLastSalon : getLastSalon
+    getLastSalon : getLastSalon,
+    getAllUsers : getAllUsers,
+    getMessageFromSalon : getMessageFromSalon,
+    getMessageFromUserInSalon : getMessageFromUserInSalon,
+    getAllMessagesFromUser : getAllMessagesFromUser
 };
 
